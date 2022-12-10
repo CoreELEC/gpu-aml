bbb9765420b04c1d51940871ca47f586fb112d92
Dezhi Kong <dezhi.kong@amlogic.com>
2022-12-10 21:37:58 +0800

    valhall: use ion instead of alloc_page for gpu [1/1]
    PD#SWPL-104787
    
    Problem:
    T5M DMC0 access fail
    
    Solution:
    force alloc from DMC1 by ion
    
    Verify:
    T5M-AY309
    
    Change-Id: I2ba39e8edd9afd190081bb560f20626d0921142d
    Signed-off-by: Dezhi Kong <dezhi.kong@amlogic.com>

--- valhall/r37p0/kernel/drivers/gpu/arm/midgard/mali_kbase_native_mgm.c
+++ valhall/r37p0/kernel/drivers/gpu/arm/midgard/mali_kbase_native_mgm.c
@@ -23,9 +23,20 @@
 #include <linux/mm.h>
 #include <linux/memory_group_manager.h>
 
+#define ION_FREE_DBG 0
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
+#include <linux/dma-buf.h>
+#include <linux/amlogic/ion.h>
+#include <linux/spinlock.h>
+unsigned int meson_ion_cma_heap_id_get(void);
+#define ION_FLAG_EXTEND_MESON_HEAP          BIT(30)
+#endif
+
 #include <mali_kbase.h>
 #include <mali_kbase_native_mgm.h>
 
+LIST_HEAD(cma_list);
+static DEFINE_SPINLOCK(cma_list_lock);
 /**
  * kbase_native_mgm_alloc - Native physical memory allocation method
  *
@@ -61,7 +72,42 @@ static struct page *kbase_native_mgm_alloc(
 	CSTD_UNUSED(mgm_dev);
 	CSTD_UNUSED(group_id);
 
+	/* temp work-around for t5m */
+	#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
+	struct dma_buf *dmabuf = NULL;
+	struct ion_buffer *buffer;
+	struct page *ret = NULL;
+	unsigned int id, size;
+	phys_addr_t paddr;
+	unsigned long flags;
+	size = PAGE_SIZE << order;
+	id = meson_ion_cma_heap_id_get();
+	if (id)
+		dmabuf = ion_alloc(size, (1 << id),
+				ION_FLAG_EXTEND_MESON_HEAP);
+	if (IS_ERR_OR_NULL(dmabuf)) {
+		printk("%s: Failed to alloc ion-cma", __func__);
+		return NULL;
+	}
+	buffer = (struct ion_buffer *)dmabuf->priv;
+	sg_dma_address(buffer->sg_table->sgl) = sg_phys(buffer->sg_table->sgl);
+	ret = sg_page(buffer->sg_table->sgl);
+	struct memory_group_cma *cma_group;
+	cma_group = kzalloc(sizeof(struct memory_group_cma), GFP_KERNEL);
+	cma_group->dmabuf = dmabuf;
+	cma_group->page = ret;
+	INIT_LIST_HEAD(&cma_group->list);
+	spin_lock_irqsave(&cma_list_lock, flags);
+	list_add_tail(&cma_group->list, &cma_list);
+	spin_unlock_irqrestore(&cma_list_lock, flags);
+	#if ION_FREE_DBG
+	printk("%s: ion_alloc dmabuf=0x%px, cg=0x%px, page=0x%px",
+		__func__, dmabuf, cma_group, ret);
+	#endif
+	return ret;
+	#else
 	return alloc_pages(gfp_mask, order);
+	#endif
 }
 
 /**
@@ -83,8 +129,31 @@ static void kbase_native_mgm_free(struct memory_group_manager_device *mgm_dev,
 {
 	CSTD_UNUSED(mgm_dev);
 	CSTD_UNUSED(group_id);
-
+	unsigned long flags;
+	/* temp work-around for t5m */
+	#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
+	struct memory_group_cma *pos = NULL, *tmp = NULL;
+	#if ION_FREE_DBG
+	printk("%s: page=0x%px", __func__, page);
+	#endif
+	spin_lock_irqsave(&cma_list_lock, flags);
+	list_for_each_entry_safe(pos, tmp, &cma_list, list) {
+		if (pos->page == page) {
+			#if ION_FREE_DBG
+			printk("%s: dma_buf_put 0x%px, cg=0x%px",
+				__func__, pos->dmabuf, pos);
+			#endif
+			dma_buf_put(pos->dmabuf);
+			list_del(&pos->list);
+			pos->dmabuf = NULL;
+			pos->page = NULL;
+			kfree(pos);
+		}
+	}
+	spin_unlock_irqrestore(&cma_list_lock, flags);
+	#else
 	__free_pages(page, order);
+	#endif
 }
 
 /**
