5a0388090d489204b023b09c403879c34321e2de
xiaotao.wei <xiaotao.wei@amlogic.com>
2024-10-14 01:55:16 +0000

    gpu: apply CVE-2023-3889 patch [1/1]
    PD#SWPL-187964
    
    Problem:
    cve patch update
    GPUCORE-38732 Continue FLUSH_MEM if GPU2019_3901 WA fails.
    
    Solution:
    apply patch CVE-2023-3889-GPUCORE-38732-GPUSWERRATA-1814-r44.diff
    If waiting for the cores power transition in GPU2019_3901 WA function,
    apply_hw_issue_GPU2019_3901_wa() fails the data flushed from LSC might
    still remain in L2. A work for GPU-reset to recover is enqueued so the
    reset will be executed asynchronously later. The WA function returns
    error code but the caller just returns without flushing L2 cache.
    
    Consequently this failure (or incompleteness) of GPU cache maintenance
    at the WA function could result in memory corruption (use-after-free)
    if the physical pages are returned to the system before the completion
    of GPU-reset and the pages get reallocated for other purposes as the
    eviction of dirty cachelines in L2 can happen before the GPU reset.
    
    apply cve patch: CVE-2023-3889-GPUCORE-38732-GPUSWERRATA-1814-r44.diff
    This patch continues FLUSH_MEM operation at WA caller to make sure
    drity data caches flused before returning to the caller and increases
    the timeout value for the core power transition.
    
    Verify:
    t5m
    
    Change-Id: I926582270dd2cea3f01118d628faa539e7e24ed1
    Signed-off-by: xiaotao.wei <xiaotao.wei@amlogic.com>

--- valhall/r44p0/kernel/drivers/gpu/arm/midgard/mmu/mali_kbase_mmu_hw_direct.c
+++ valhall/r44p0/kernel/drivers/gpu/arm/midgard/mmu/mali_kbase_mmu_hw_direct.c
@@ -253,7 +253,7 @@ static int wait_l2_power_trans_complete(struct kbase_device *kbdev)
 #if !IS_ENABLED(CONFIG_MALI_NO_MALI)
 static int wait_cores_power_trans_complete(struct kbase_device *kbdev)
 {
-#define WAIT_TIMEOUT 1000 /* 1ms timeout */
+#define WAIT_TIMEOUT 50000 /* 50ms timeout */
 #define DELAY_TIME_IN_US 1
 	const int max_iterations = WAIT_TIMEOUT;
 	int loop;
@@ -273,7 +273,9 @@ static int wait_cores_power_trans_complete(struct kbase_device *kbdev)
 	}
 
 	if (loop == max_iterations) {
-		dev_warn(kbdev->dev, "SHADER_PWRTRANS set for too long");
+		dev_warn(kbdev->dev, "SHADER_PWRTRANS %08x%08x set for too long",
+			kbase_reg_read(kbdev, GPU_CONTROL_REG(SHADER_PWRTRANS_HI)),
+			kbase_reg_read(kbdev, GPU_CONTROL_REG(SHADER_PWRTRANS_LO)));
 		return -ETIMEDOUT;
 	}
 
@@ -306,9 +308,8 @@ static int apply_hw_issue_GPU2019_3901_wa(struct kbase_device *kbdev, u32 *mmu_c
 	 * the workaround can be safely skipped.
 	 */
 	if (kbdev->pm.backend.l2_state != KBASE_L2_OFF) {
-		if (*mmu_cmd != AS_COMMAND_FLUSH_MEM) {
-			dev_warn(kbdev->dev,
-				 "Unexpected mmu command received");
+		if (unlikely(*mmu_cmd != AS_COMMAND_FLUSH_MEM)) {
+			dev_warn(kbdev->dev, "Unexpected MMU command(%u) received", *mmu_cmd);
 			return -EINVAL;
 		}
 
@@ -602,8 +603,14 @@ static int mmu_hw_do_flush(struct kbase_device *kbdev, struct kbase_as *as,
 			ret = apply_hw_issue_GPU2019_3901_wa(kbdev, &mmu_cmd, as->number);
 		}
 
-		if (ret)
-			return ret;
+		if (ret) {
+			dev_warn(kbdev->dev,
+				 "Failed to apply WA for HW issue when doing MMU flush op on VA range %llx-%llx for AS %u",
+				 op_param->vpfn << PAGE_SHIFT,
+				 ((op_param->vpfn + op_param->nr) << PAGE_SHIFT) - 1,
+				 as->number);
+			/* Continue with the MMU flush operation */
+		}
 	}
 #endif
 
