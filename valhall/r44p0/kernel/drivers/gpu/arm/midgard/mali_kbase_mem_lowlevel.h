683e2d8b54a3b8888bf37222d360702a9c449b18
xiaotao.wei <xiaotao.wei@amlogic.com>
2024-10-14 01:55:16 +0000

    gpu: apply CVE-2024-4607 patch [1/1]
    PD#SWPL-187964
    
    Problem:
    cve patch update
    GPUCORE-41547 Improve handling of page metadata after migration
    
    Solution:
    apply patch CVE-2024-4607-r50p0.diff
    Handling of page metadata after migration in kbase_mmu_migrate_page()
    has been made slightly safer. Only if the page is still in the
    ALLOCATED_MAPPED state is it safe to access and update the
    metadata. There's no problem if the page became NOT_MOVABLE.
    It is an error, instead, if the page has changed to any other
    state.
    
    The kbase_set_phy_alloc_page_status() helper must be called
    while keeping the region lock to prevent race conditions where
    the pages of a GPU VA region could be set to NOT_MOVABLE
    while a page migration is in progress.
    
    Verify:
    t5m
    
    Change-Id: I6229697fd5ca3c4fc27839481c7731c0b023a1b2
    Signed-off-by: xiaotao.wei <xiaotao.wei@amlogic.com>

--- valhall/r44p0/kernel/drivers/gpu/arm/midgard/mali_kbase_mem_lowlevel.h
+++ valhall/r44p0/kernel/drivers/gpu/arm/midgard/mali_kbase_mem_lowlevel.h
@@ -28,6 +28,10 @@
 
 #include <linux/dma-mapping.h>
 
+/* Kernel/CPU page size can be 4/16/64KB whereas GPU page size is fixed as 4KB */
+#define GPU_PAGE_SIZE SZ_4K
+#define GPU_PAGES_PER_CPU_PAGE (PAGE_SIZE / GPU_PAGE_SIZE)
+
 /* Flags for kbase_phy_allocator_pages_alloc */
 #define KBASE_PHY_PAGES_FLAG_DEFAULT (0)	/** Default allocation flag */
 #define KBASE_PHY_PAGES_FLAG_CLEAR   (1 << 0)	/** Clear the pages after allocation */
